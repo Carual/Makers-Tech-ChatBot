191eb9719a04a5b058ba5149bc22fd951e3dec44
Carual <juan.sartru@gmail.com>
2024-11-14 17:44:29 -0500

    prompt test

--- server/services/chatbotService.ts
+++ server/services/chatbotService.ts
@@ -5,20 +5,25 @@ const client = new Groq({
 })
 
 async function generateQueryWithGPT(question: string) {
-	const chatCompletion = await client.chat.completions.create({
-		messages: [
-			{
-				role: 'system',
-				content: `Transform this question into a mongodb query with the schema being {name: String, type: String, brand: String, quantity: Number, description: String, price: Number} only respond in this format:{operation: 'find' | 'count',filters: any`,
-			},
-			{ role: 'user', content: question },
-		],
-		model: 'llama-3.1-8b-instant',
-	})
-	return chatCompletion.choices[0].message.content
+	try {
+		const chatCompletion = await client.chat.completions.create({
+			messages: [
+				{
+					role: 'system',
+					content: `You are a chatbot to help users ask questions about products in a database transform this question into a mongodb query with the schema being {name: String, type: String, brand: String, quantity: Number, description: String, price: Number} only respond in this format:{operation: 'find' | 'count' | 'conversation',filters: any} if the question is not about products in the database respond in this format: {operation: 'conversation', filters: 'string'} 'string' is the answer to the question.`,
+				},
+				{ role: 'user', content: question },
+			],
+			model: 'llama-3.1-8b-instant',
+		})
+		console.log('object :>> ', chatCompletion.choices[0].message.content)
+		return JSON.parse(chatCompletion.choices[0].message.content as string)
+	} catch (error) {
+		return {
+			operation: 'conversation',
+			filters: 'Lo siento, no pude entender tu pregunta. Por favor, inténtalo de nuevo.',
+		}
+	}
 }
 
-
-
-
-console.log(await generateQueryWithGPT('Cuantos productos hay en la base de datos de marca Dell?'))
+console.log(await generateQueryWithGPT('Cuantos computadores hay?'))
